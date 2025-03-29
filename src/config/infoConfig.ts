86272735cbddffc3fb977a042e1d885091f7c4a1
Akachi-Guo <YX1349791010@outlook.com>
2025-03-29 22:52:18 +0800

    修改了自我介绍和标题，用来测试发布的可能性

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,10 +3,10 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Akachi Guo'
+export const headline = 'Undergraduate at Northeastern University,majoring in Bioengineering.'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
+  "Welcome to my personal page!I am Akachi Guo,a bioengineering undergraduate at Northeastern University in Shenyang,Liaoning.Hailing from the beautiful province of Jiangxi,I am currently immersed in the fascinating world of bioengineering,exploring the intersection of biology and technology.I am delighted to connect with you through this platform and look forward to sharing my journey and experiences with you."
 export const email = 'tong.hu@scls-sh.org'
 export const githubUsername = 'derekhut'
 
